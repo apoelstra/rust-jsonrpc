0496b1b25ca22b8f948a66375ff77693a456ed97
Steven Roose <steven@stevenroose.org>
2020-12-14 14:20:04 +0000

    Replace HttpRoundtripper with Transport
    Which doesn't rely on HTTP to work.

--- src/lib.rs
+++ src/lib.rs
@@ -28,8 +28,8 @@
 #![deny(unused_mut)]
 #![warn(missing_docs)]
 
-extern crate base64;
-extern crate http;
+// extern crate base64;
+// extern crate http;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
@@ -42,24 +42,24 @@ mod util;
 // Re-export error type
 pub use error::Error;
 
-#[derive(Debug, Clone, PartialEq, Serialize)]
+#[derive(Debug, Clone, Serialize)]
 /// A JSONRPC request object
-pub struct Request<'a, 'b> {
+pub struct Request<'a> {
     /// The name of the RPC call
     pub method: &'a str,
     /// Parameters to the RPC call
-    pub params: &'b [serde_json::Value],
+    pub params: &'a [Box<serde_json::value::RawValue>],
     /// Identifier for this Request, which should appear in the response
     pub id: serde_json::Value,
     /// jsonrpc field, MUST be "2.0"
     pub jsonrpc: Option<&'a str>,
 }
 
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 /// A JSONRPC response object
 pub struct Response {
     /// A result if there is one, or null
-    pub result: Option<serde_json::Value>,
+    pub result: Option<Box<serde_json::value::RawValue>>,
     /// An error if there is one, or null
     pub error: Option<error::RpcError>,
     /// Identifier for this Request, which should match that of the request
@@ -70,22 +70,16 @@ pub struct Response {
 
 impl Response {
     /// Extract the result from a response
-    pub fn result<T: serde::de::DeserializeOwned>(&self) -> Result<T, Error> {
+    pub fn result<T: for<'a> serde::de::Deserialize<'a>>(&self) -> Result<T, Error> {
         if let Some(ref e) = self.error {
             return Err(Error::Rpc(e.clone()));
         }
 
-        serde_json::from_value(self.result.clone().unwrap_or(serde_json::Value::Null))
-            .map_err(Error::Json)
-    }
-
-    /// Extract the result from a response, consuming the response
-    pub fn into_result<T: serde::de::DeserializeOwned>(self) -> Result<T, Error> {
-        if let Some(e) = self.error {
-            return Err(Error::Rpc(e));
+        if let Some(ref res) = self.result {
+            serde_json::from_str(res.get()).map_err(Error::Json)
+        } else {
+            serde_json::from_value(serde_json::Value::Null).map_err(Error::Json)
         }
-
-        serde_json::from_value(self.result.unwrap_or(serde_json::Value::Null)).map_err(Error::Json)
     }
 
     /// Return the RPC error, if there was one, but do not check the result
@@ -108,11 +102,12 @@ mod tests {
 
     use super::Response;
     use serde_json;
+    use serde_json::value::RawValue;
 
     #[test]
     fn response_is_none() {
         let joanna = Response {
-            result: Some(From::from(true)),
+            result: Some(RawValue::from_string(serde_json::to_string(&true).unwrap()).unwrap()),
             error: None,
             id: From::from(81),
             jsonrpc: Some(String::from("2.0")),
@@ -133,14 +128,14 @@ mod tests {
     fn response_extract() {
         let obj = vec!["Mary", "had", "a", "little", "lamb"];
         let response = Response {
-            result: Some(serde_json::to_value(&obj).unwrap()),
+            result: Some(RawValue::from_string(serde_json::to_string(&obj).unwrap()).unwrap()),
             error: None,
             id: serde_json::Value::Null,
             jsonrpc: Some(String::from("2.0")),
         };
         let recovered1: Vec<String> = response.result().unwrap();
         assert!(response.clone().check_error().is_ok());
-        let recovered2: Vec<String> = response.into_result().unwrap();
+        let recovered2: Vec<String> = response.result().unwrap();
         assert_eq!(obj, recovered1);
         assert_eq!(obj, recovered2);
     }
@@ -150,12 +145,12 @@ mod tests {
         let s = r#"{"result":null,"error":null,"id":"test"}"#;
         let response: Response = serde_json::from_str(&s).unwrap();
         let recovered1: Result<(), _> = response.result();
-        let recovered2: Result<(), _> = response.clone().into_result();
+        let recovered2: Result<(), _> = response.clone().result();
         assert!(recovered1.is_ok());
         assert!(recovered2.is_ok());
 
         let recovered1: Result<String, _> = response.result();
-        let recovered2: Result<String, _> = response.clone().into_result();
+        let recovered2: Result<String, _> = response.clone().result();
         assert!(recovered1.is_err());
         assert!(recovered2.is_err());
     }
