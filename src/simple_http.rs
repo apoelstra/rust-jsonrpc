138c2e47a290c24a2f820611320eed83d28c3d6b
Tobin C. Harding <me@tobin.cc>
2022-06-09 13:26:39 +1000

    Remove redundant slicing of whole range
    Clippy emits:
    
     warning: redundant slicing of the whole range
    
    Remove redundant slicing of whole range.

--- src/simple_http.rs
+++ src/simple_http.rs
@@ -326,7 +326,7 @@ impl Builder {
         let mut auth = user.as_ref().to_owned();
         auth.push(':');
         if let Some(ref pass) = pass {
-            auth.push_str(&pass.as_ref()[..]);
+            auth.push_str(&pass.as_ref());
         }
         self.tp.basic_auth = Some(format!("Basic {}", &base64::encode(auth.as_bytes())));
         self
