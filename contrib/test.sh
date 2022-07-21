97ad9562c83ba7725fc871d1069f2ea4cd44212c
Tobin C. Harding <me@tobin.cc>
2022-07-21 14:06:55 +1000

    Add DO_FEATURE_MATRIX
    Add an env var `DO_FEATURE_MATRIX` and when it is set test each of the
    features individually.
    
    Add each of the three transport features to the features list.

--- contrib/test.sh
+++ contrib/test.sh
@@ -1,14 +1,27 @@
 #!/bin/sh -ex
 
-FEATURES=""
+FEATURES="simple_http simple_tcp simple_uds"
 
-# Test without any features first
-cargo test --verbose
+cargo --version
+rustc --version
 
-# Test each feature
-for feature in ${FEATURES}
-do
-    cargo test --verbose --features="$feature"
-done
+# Defaults / sanity checks
+cargo build --all
+cargo test --all
+
+if [ "$DO_FEATURE_MATRIX" = true ]; then
+    cargo build --no-default-features
+    cargo test --no-default-features
+
+    # All features
+    cargo build --no-default-features --features="$FEATURES"
+    cargo test --no-default-features --features="$FEATURES"
+
+    # Single features
+    for feature in ${FEATURES}
+    do
+        cargo test --no-default-features --features="$feature"
+    done
+fi
 
 exit 0
